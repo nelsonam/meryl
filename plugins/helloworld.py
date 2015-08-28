f1cb06856ae458d768997a6a4cb902b693db4b56
Allison Nelson <allison.nelson13@gmail.com>
2015-08-27 21:36:06 -0700

    add \!goodbye and \!bot commands

--- plugins/helloworld.py
+++ plugins/helloworld.py
@@ -4,3 +4,11 @@ class HelloWorld(BotPlugin):
     @botcmd
     def hello(self, msg, args):
         return "Hello, livecoding.tv!"
+
+    @botcmd
+    def goodbye(self, msg, args):
+        return "Goodbye, cruel world!"
+
+    @botcmd
+    def bot(self, msg, arg):
+        return "Hello, I am Meryl. I was created by allisonanalytics and singlerider."
